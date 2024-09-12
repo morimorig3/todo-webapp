880fd03a69290e2164923b798e6d4cf7f17564ad
morimorig3 <mori.nto@outlook.jp>
2024-09-12 09:40:57 +0900

    feat:Todoのモデルを定義

--- main.go
+++ main.go
@@ -2,11 +2,21 @@ package main
 
 import (
 	"net/http"
+	"time"
 
 	"github.com/labstack/echo/v4"
 )
 
-func main()  {
+type Todo struct {
+	ID        int64
+	Content   string
+	Done      bool
+	Until     time.Time
+	CreatedAt time.Time
+}
+
+func main() {
+
 	e := echo.New()
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "")
