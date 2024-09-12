d08559b8b0a4fee67b11b289bed2b70a63ba1f56
morimorig3 <mori.nto@outlook.jp>
2024-09-12 09:50:23 +0900

    feat:bunによるModel定義

--- main.go
+++ main.go
@@ -5,18 +5,22 @@ import (
 	"time"
 
 	"github.com/labstack/echo/v4"
+	"github.com/uptrace/bun"
 )
 
 type Todo struct {
-	ID        int64
-	Content   string
-	Done      bool
-	Until     time.Time
+	bun.BaseModel `bun:"table:todos,alias:t"`
+
+	ID        int64     `bun:"id,pk,autoincrement"`
+	Content   string    `bun:"content,notnull"`
+	Done      bool      `bun:"done"`
+	Until     time.Time `bun:"until,nullzero"`
 	CreatedAt time.Time
+	UpdatedAt time.Time `bun:",nullzero"`
+	DeletedAt time.Time `bun:",soft_delete,nullzero"`
 }
 
 func main() {
-
 	e := echo.New()
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "")
